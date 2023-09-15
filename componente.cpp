ecd0344699cf0da1b3a5389bb2f36edd0e3cbd87
00Urbina00 <edurbina2002@hotmail.com>
2023-09-14 22:43:04 -0600

    Cmake ya no se sincroniza

--- componente.cpp
+++ componente.cpp
@@ -5,7 +5,7 @@ Componente::Componente(const std::string& lexema, const std::string& token){
     id = -1;
     tipos_de_datos = {"int", "float", "void"};
     palabras_reservadas = {
-        "case", "break", "continue", "return", "class",
+        "case", "break", "continue", "return", "class", "printf",
         "public", "private", "protected", "const", "try", "except", "finally", "include",
         "new", "delete", "namespace", "template", "using", "typedef", "sizeof", "static",
         "virtual", "friend", "operator", "struct", "union", "this", "true", "false"
